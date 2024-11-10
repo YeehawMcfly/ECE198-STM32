064bfa5fed2393aa9ab55ec0daf4a3cf70eb3feb
Jinyuan (Eugene) Zhao <eugenezhao63@gmail.com>
2024-11-10 14:58:52 -0500

    Changed to half duplex

--- Receiver/Sending Signals/Core/Src/main.c
+++ Receiver/Sending Signals/Core/Src/main.c
@@ -26,7 +26,11 @@ int main(void){
     SystemClock_Config();
     MX_GPIO_Init();
     MX_USART1_UART_Init();
+
+    // Initialize UART Reception in Half-Duplex Mode
+    HAL_HalfDuplex_EnableReceiver(&huart1);
     HAL_UARTEx_ReceiveToIdle_IT(&huart1, RxData, sizeof(RxData));
+
     while(1){}
 }
 
@@ -36,10 +40,10 @@ static void MX_USART1_UART_Init(void){
     huart1.Init.WordLength = UART_WORDLENGTH_8B;
     huart1.Init.StopBits = UART_STOPBITS_1;
     huart1.Init.Parity = UART_PARITY_NONE;
-    huart1.Init.Mode = UART_MODE_TX_RX; // Changed from RX to TX_RX
+    huart1.Init.Mode = UART_MODE_TX_RX; // Enable both TX and RX
     huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
     huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-    if(HAL_UART_Init(&huart1)!= HAL_OK){
+    if (HAL_HalfDuplex_Init(&huart1) != HAL_OK){
         Error_Handler();
     }
 }
@@ -50,11 +54,13 @@ static void MX_GPIO_Init(void){
     __HAL_RCC_GPIOH_CLK_ENABLE();
     __HAL_RCC_GPIOA_CLK_ENABLE();
 
+    // Configure PC13 as Input with Interrupt
     GPIO_InitStruct.Pin = GPIO_PIN_13;
     GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
     GPIO_InitStruct.Pull = GPIO_PULLUP;
     HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
+    // Enable and Set EXTI Line 15_10 Interrupt to the lowest priority
     HAL_NVIC_SetPriority(EXTI15_10_IRQn,0,0);
     HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 }
@@ -63,23 +69,27 @@ void SystemClock_Config(void){
     RCC_OscInitTypeDef RCC_OscInitStruct = {0};
     RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
+    // Configure the main internal regulator output voltage
     __HAL_RCC_PWR_CLK_ENABLE();
     __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
 
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-    RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+    // Initialize RCC Oscillators
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
     RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLM = 4;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLM = 8;
     RCC_OscInitStruct.PLL.PLLN = 84;
     RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
     RCC_OscInitStruct.PLL.PLLQ = 4;
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct)!= HAL_OK){
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK){
         Error_Handler();
     }
 
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
-                                  RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    // Initialize CPU, AHB and APB Clocks
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                                |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
     RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
     RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
@@ -91,7 +101,7 @@ void SystemClock_Config(void){
 
 void Error_Handler(void){
     __disable_irq();
-    while(1){}
+    while (1){}
 }
 
 #ifdef USE_FULL_ASSERT
