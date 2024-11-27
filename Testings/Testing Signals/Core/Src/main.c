82abe6ba0caa70ed011d06a62e6c74ec0cd90bd4
say-no-to-sleep <eugenezhao63@gmail.com>
2024-11-26 21:57:01 -0500

    Added files from main branch

---
+++ Testings/Testing Signals/Core/Src/main.c
@@ -0,0 +1,258 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body with encryption
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stdio.h"
+#include "string.h"
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart1;
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
+void encrypt(uint32_t v[2], const uint32_t k[4]);
+void decrypt(uint32_t v[2], const uint32_t k[4]);
+void encryptMessage(uint8_t* input, size_t len);
+void decryptMessage(uint8_t* input, size_t len);
+
+/* USER CODE BEGIN PV */
+uint8_t RxData[32];  // Increased buffer size to accommodate encrypted data
+int indx = 0;
+uint8_t TxData[32];  // Increased buffer size
+int isClicked = 0;
+
+// Encryption key - should be same on both devices
+const uint32_t key[4] = { 1, 2, 3, 4 };
+
+// Buffer for encryption/decryption operations
+uint32_t data_buffer[4];
+
+/* USER CODE BEGIN PV */
+
+void encrypt(uint32_t v[2], const uint32_t k[4]) {
+    uint32_t v0=v[0], v1=v[1], sum=0, i;
+    uint32_t delta=0x9E3779B9;
+    uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];
+    for (i=0; i<32; i++) {
+        sum += delta;
+        v0 += ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1);
+        v1 += ((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>5) + k3);
+    }
+    v[0]=v0; v[1]=v1;
+}
+
+void decrypt(uint32_t v[2], const uint32_t k[4]) {
+    uint32_t v0=v[0], v1=v[1], sum=0xC6EF3720, i;
+    uint32_t delta=0x9E3779B9;
+    uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];
+    for (i=0; i<32; i++) {
+        v1 -= ((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>5) + k3);
+        v0 -= ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1);
+        sum -= delta;
+    }
+    v[0]=v0; v[1]=v1;
+}
+
+void encryptMessage(uint8_t* input, size_t len) {
+    // Clear the buffer first
+    memset(data_buffer, 0, sizeof(data_buffer));
+
+    // Copy input data to buffer
+    memcpy(data_buffer, input, len);
+
+    // Encrypt in blocks of 8 bytes (2 uint32_t)
+    for (int i = 0; i < 4; i += 2) {
+        encrypt(&data_buffer[i], key);
+    }
+
+    // Copy back to input buffer
+    memcpy(input, data_buffer, sizeof(data_buffer));
+}
+
+void decryptMessage(uint8_t* input, size_t len) {
+    // Copy input to buffer
+    memcpy(data_buffer, input, len);
+
+    // Decrypt in blocks of 8 bytes
+    for (int i = 0; i < 4; i += 2) {
+        decrypt(&data_buffer[i], key);
+    }
+
+    // Copy back to input buffer
+    memcpy(input, data_buffer, sizeof(data_buffer));
+}
+
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
+    if (Size > 0) {
+        // Decrypt the received data
+        decryptMessage(RxData, Size);
+        // Re-enable reception
+        HAL_UARTEx_ReceiveToIdle_IT(&huart1, RxData, 32);
+    }
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+    if (GPIO_Pin == GPIO_PIN_13) {
+        if (isClicked == 0) {
+            isClicked = 1;
+        }
+    }
+}
+
+int main(void) {
+    /* Standard initialization code remains the same until while(1) loop */
+    HAL_Init();
+    SystemClock_Config();
+    MX_GPIO_Init();
+    MX_USART1_UART_Init();
+
+    HAL_HalfDuplex_EnableReceiver(&huart1);
+    HAL_UARTEx_ReceiveToIdle_IT(&huart1, RxData, 32);
+
+    while (1) {
+        if (isClicked == 1) {
+            HAL_Delay(500);
+            indx++;
+
+            // Prepare message
+            int len = sprintf((char*)TxData, "Hello From f401--%d", indx);
+
+            // Encrypt the message
+            encryptMessage(TxData, 32);  // Use full buffer size for encryption
+
+            // Send encrypted data
+            HAL_HalfDuplex_EnableTransmitter(&huart1);
+            HAL_UART_Transmit(&huart1, TxData, 32, 1000);  // Send full buffer
+            HAL_HalfDuplex_EnableReceiver(&huart1);
+
+            isClicked = 0;
+        }
+    }
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 84;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_HalfDuplex_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  __disable_irq();
+  while (1)
+  {
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+}
+#endif /* USE_FULL_ASSERT */
